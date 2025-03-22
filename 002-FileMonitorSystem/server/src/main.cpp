125d31b7aab041c01332da532c510313beb37813
strongercjd <strongercjd@163.com>
2025-03-23 07:44:15 +0800

    修改为智能指针

--- 002-FileMonitorSystem/server/src/main.cpp
+++ 002-FileMonitorSystem/server/src/main.cpp
@@ -2,9 +2,10 @@
 #include "FileWatcher.hpp"
 #include <csignal>
 #include <iostream>
+#include <memory>
 
-IPCManager* g_ipcManager = nullptr;
-FileWatcher* g_fileWatcher = nullptr;
+std::unique_ptr<IPCManager> g_ipcManager;
+std::unique_ptr<FileWatcher> g_fileWatcher;
 
 void signalHandler(int signum) {
     if (g_fileWatcher) {
@@ -21,16 +22,20 @@ int main() {
     const std::string socketPath = "/tmp/ipc_socket.sock";
     
     try {
-        // 初始化IPC管理器
-        IPCManager manager(socketPath);
-        g_ipcManager = &manager;
+        /*
+        不使用全局变量的原因是：
+        1. 初始化顺序问题:全局变量的初始化顺序是不确定的，这可能导致依赖关系的问题。
+        IPCManager manager(socketPath);  // 这里构造会立即执行，但此时可能其他全局变量尚未初始化
+        FileWatcher watcher(manager);   // 这里构造会依赖manager，但此时manager可能尚未初始化
+        2. 异常安全：当前设计将对象的构造放在try块中，可以更好地捕获构造函数抛出的异常
+        3. 全局变量的初始化和销毁是在程序启动和结束时进行的，而不是在函数调用时进行的。
+        */
+        g_ipcManager = std::make_unique<IPCManager>(socketPath);// 初始化IPC管理器
+        g_fileWatcher = std::make_unique<FileWatcher>(*g_ipcManager);// 初始化文件监控
         
-        // 初始化文件监控
-        FileWatcher watcher(manager);
-        g_fileWatcher = &watcher;
         try
         {
-            watcher.addWatch(PROJECT_ROOT"/server/configs/app.json");
+            g_fileWatcher->addWatch(PROJECT_ROOT"/server/configs/app.json");
         }
         catch(const std::exception& e)
         {
@@ -44,11 +49,14 @@ int main() {
         signal(SIGTERM, signalHandler);
         
         // 启动服务
-        watcher.start();
-        manager.start();
+        g_fileWatcher->start();
+        g_ipcManager->start();
         
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
+         // 清理资源
+         g_fileWatcher.reset();
+         g_ipcManager.reset();
         return EXIT_FAILURE;
     }
     
